5a932d1ea64ece8f236ed07e9ae9aad321437438
OnalisSilano <onatoros@outlook.com>
2024-02-22 07:33:27 +0900

    add Row

--- codes/DiceRoller2/app/src/main/java/com/example/diceroller2/MainActivity.kt
+++ codes/DiceRoller2/app/src/main/java/com/example/diceroller2/MainActivity.kt
@@ -5,13 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -51,17 +62,51 @@ fun DiceWithButtonAndImage(
         .fillMaxSize()
         .wrapContentSize(Alignment.Center)
 ) {
+    var result by rememberSaveable {
+        mutableIntStateOf(1)
+    }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    val diceRollText = when(result) {
+        1 -> R.string.roll_text_1
+        2 -> R.string.roll_text_2
+        3 -> R.string.roll_text_3
+        4 -> R.string.roll_text_4
+        5 -> R.string.roll_text_5
+        else -> R.string.roll_text_6
+    }
+
     Column(
-        modifier = modifier,
+        modifier = modifier.verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Row(
+            modifier = Modifier.padding(bottom = 32.dp,),
+            verticalAlignment = Alignment.Top,
+        ) {
+            Text(
+                text = stringResource(id = R.string.roll_text_label),
+                style = MaterialTheme.typography.displayLarge,
+                )
+            Text(
+                modifier = Modifier.padding(horizontal = 8.dp,),
+                text = stringResource(id = diceRollText),
+                style = MaterialTheme.typography.displayLarge,
+                )
+        }
         Image(
-            painter = painterResource(id = R.drawable.dice_1),
-            contentDescription = "1"
+            painter = painterResource(id = imageResource),
+            contentDescription = result.toString(),
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = { /*TODO*/ }
+            onClick = { result = (1..6).random() }
         ) {
             Text(text = stringResource(id = R.string.roll))
         }
