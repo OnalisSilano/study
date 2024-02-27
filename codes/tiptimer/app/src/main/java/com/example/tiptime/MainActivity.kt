dba8618567e3d7dc76b69abf38a70ae3468a694d
OnalisSilano <onatoros@outlook.com>
2024-02-27 23:46:56 +0900

    すうちのにゅうりょくらんをついか

--- codes/tiptimer/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ codes/tiptimer/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -71,6 +72,7 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
+        EditNumberField()
         Text(
             text = stringResource(R.string.tip_amount, "$0.00"),
             style = MaterialTheme.typography.displaySmall
@@ -87,6 +89,15 @@ fun TipTimeLayout() {
 private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+@Composable
+fun EditNumberField(){
+    val amountInput = "0"
+    TextField(
+        value = amountInput,
+        onValueChange = {}
+    )
 }
 
 @Preview(showBackground = true)
