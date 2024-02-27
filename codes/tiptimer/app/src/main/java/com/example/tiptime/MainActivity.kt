1727efb7b63ff4a43e88c70d1d0c72773de98890
OnalisSilano <onatoros@outlook.com>
2024-02-28 00:33:30 +0900

    add tip percentage

--- codes/tiptimer/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ codes/tiptimer/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -23,18 +23,26 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
@@ -58,6 +66,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
+    var amountInput by remember {
+        mutableStateOf("")
+    }
+    var tipPercentInput by remember {
+        mutableStateOf("10.0")
+    }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tipPercentAmount = tipPercentInput.toDoubleOrNull() ?: 10.0
+    val tip = calculateTip(amount,tipPercentAmount)
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -72,9 +89,19 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberField()
+        EditNumberField(
+            value = amountInput,
+            onValueChange = { amountInput = it },
+            modifier = Modifier
+                .padding(
+                    bottom = 32.dp
+                )
+                .fillMaxWidth(),
+            tipPercent = tipPercentInput,
+            tipValueChange = { tipPercentInput = it }
+        )
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -92,11 +119,37 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 }
 
 @Composable
-fun EditNumberField(){
-    val amountInput = "0"
+private fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    tipPercent: String,
+    tipValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier) {
     TextField(
-        value = amountInput,
-        onValueChange = {}
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        label = {
+            Text(
+                text = stringResource(id = R.string.bill_amount)
+            )
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+    )
+    TextField(
+        value = tipPercent,
+        onValueChange = tipValueChange,
+        modifier = modifier.padding(
+            top = 8.dp
+        ),
+        label = {
+            Text(
+                text = stringResource(id = R.string.tip_percent)
+            )
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
     )
 }
 
